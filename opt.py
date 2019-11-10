843dd7b2773d8bbcdd7ce9dca53b6700e7cc3f8f
heini-phys-chem <heini.phys.chem@gmail.com>
2019-11-10 17:07:28 +0100

    added opt information

--- opt.py
+++ opt.py
@@ -3,6 +3,7 @@
 import sys
 import numpy as np
 from scipy.optimize import minimize
+from scipy.optimize import fmin_bfgs
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -10,22 +11,22 @@ import seaborn as sns
 def rosen(x):
 	return sum(100.0*(x[1:]-x[:-1]**2.0)**2.0 + (1-x[:-1])**2.0)
 
-def rosen_der(x):
-	xm    = x[1:-1]
-	xm_m1 = x[:-2]
-	xm_p1 = x[2:]
+#def rosen_der(x):
+#	xm    = x[1:-1]
+#	xm_m1 = x[:-2]
+#	xm_p1 = x[2:]
+#
+#	der = np.zeros_like(x)
+#	der[1:-1] = 200/(xm-xm_m1**2) - 400*(xm_p1 - xm**2)*xm - 2*(1-xm)
+#	der[0]    = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
+#	der[-1]   = 200*(x[-1]-x[-2]**2)
+#
+#	return der
 
-	der = np.zeros_like(x)
-	der[1:-1] = 200/(xm-xm_m1**2) - 400*(xm_p1 - xm**2)*xm - 2*(1-xm)
-	der[0]    = -400*x[0]*(x[1]-x[0]**2) - 2*(1-x[0])
-	der[-1]   = 200*(x[-1]-x[-2]**2)
+def plot_function():
 
-	return der
-
-def plot_data():
-
-	X0 = np.arange(start=-1.5, stop=2.0, step=0.3)
-	Y0 = np.arange(start=-0.5, stop=3.0, step=0.3)
+	X0 = np.arange(start=-1.5, stop=2.0, step=0.1)
+	Y0 = np.arange(start=-0.5, stop=2.9, step=0.1)
 
 	data = np.array([])
 
@@ -34,19 +35,37 @@ def plot_data():
 			value = rosen( np.array([ float(X0[i]), float(Y0[j]) ]) )
 			data = np.append(data, float(value))
 
+	print(len(data))
 	data = data.reshape(len(X0), len(Y0))
 	sns.heatmap(data, cmap='coolwarm')
 	plt.show()
 
-def minimize(starting_point)
-	res = minimize(rosen, starting_point, method='BFGS', jac=rosen_der,options={'disp': True})
+def callbackF(Xi):
+    print("{}\t\t{}\t\t{}".format(Xi[0], Xi[1], rosen(Xi)))
+
+def minimize(starting_point):
+	#[xopt, fopt, gopt, Bopt, func_calls, grad_calls, warnflg] = fmin_bfgs(rosen, starting_point, callback=callbackF, full_output=True)
+	fmin_bfgs(rosen, starting_point)
+
+#	print()
+#	print("xopt:       {}".format(xopt))
+#	print("fopt:       {}".format(fopt))
+#	print("gopt:       {}".format(gopt))
+##	print("Bopt:       {}".format(Bopt))
+#	print("func_calls: {}".format(func_calls))
+#	print("grad_calls: {}".format(grad_calls))
+#	print("warnflg:    {}".format(warnflg))
+#	print()
 
 if __name__ == "__main__":
 
-	plot_function(X0, Y0)
+	x = float(sys.argv[1])
+	y = float(sys.argv[2])
 
-	starting_point = [-.5, .5]
+	starting_point = np.array([x, y])
 	minimize(starting_point)
+	print(x,y)
+#	plot_function()
 
 '''
 Nfeval = 1
